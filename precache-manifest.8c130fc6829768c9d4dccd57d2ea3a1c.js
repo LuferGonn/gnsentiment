0999659ee4a5a6b9b092e6e87abb4f45a1de0865
Luis Fernando <lufergonn@gmail.com>
2020-09-15 13:16:56 -0500

    App -> nutres prueba

---
+++ precache-manifest.8c130fc6829768c9d4dccd57d2ea3a1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7a16fa2b0ae541c4bf440934d2c326",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16732da6232854c9ef96",
+    "url": "/static/css/2.5c44d354.chunk.css"
+  },
+  {
+    "revision": "5c455dd4233fc04c8863",
+    "url": "/static/css/main.2bd92d45.chunk.css"
+  },
+  {
+    "revision": "16732da6232854c9ef96",
+    "url": "/static/js/2.61d286cc.chunk.js"
+  },
+  {
+    "revision": "0ed84ef73ec6382df5b068bd3e48d590",
+    "url": "/static/js/2.61d286cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c455dd4233fc04c8863",
+    "url": "/static/js/main.c37f2c72.chunk.js"
+  },
+  {
+    "revision": "c293acb16c3e7b2abc9e",
+    "url": "/static/js/runtime-main.7aced83e.js"
+  }
+]);
